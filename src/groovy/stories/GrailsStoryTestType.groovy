6ec390c53311e14524dc387b6d08413734a2f9bd
Jonathan Leibiusky <jonathan.leibiusky@mercadolibre.com>
2010-05-14 15:44:35 -0300

    Fixed dependencies

--- src/groovy/stories/GrailsStoryTestType.groovy
+++ src/groovy/stories/GrailsStoryTestType.groovy
@@ -1,6 +1,10 @@
 package stories
 
-import org.codehaus.groovy.grails.test.support.GrailsTestTypeSupport;
+import org.codehaus.groovy.grails.test.GrailsTestTypeResult
+import org.codehaus.groovy.grails.test.support.GrailsTestTypeSupport
+import org.codehaus.groovy.grails.test.event.GrailsTestEventPublisher
+import org.codehaus.groovy.grails.test.GrailsTestTargetPattern
+import stories.GrailsStoryTestTypeResult
 
 class GrailsStoryTestType extends GrailsTestTypeSupport {
     GrailsStoryTestType(String name, String relativeSourcePath) {
@@ -16,13 +20,16 @@ class GrailsStoryTestType extends GrailsTestTypeSupport {
     }
 
     protected int doPrepare() {
-        eachSource { pattern, file ->
-            println file
+        for (GrailsTestTargetPattern targetPattern : this.getTestTargetPatterns()) {
+            for (File file : findSourceFiles(targetPattern)) {
+                println file
+            }
         }
+        //TODO: return the real number of tests
         return 1;
     }
 
     GrailsTestTypeResult doRun(GrailsTestEventPublisher eventPublisher) {
-        return new StoriesGrailsTestTypeResult()    
+        return new GrailsStoryTestTypeResult()    
     }
 }
