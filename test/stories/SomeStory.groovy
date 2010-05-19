c3365d8ee0d6462afd4915a0eefbf761a48580ac
Jonathan Leibiusky <jonathan.leibiusky@mercadolibre.com>
2010-05-19 10:55:13 -0300

    Changed story to fail with a more elegant message

--- test/stories/SomeStory.groovy
+++ test/stories/SomeStory.groovy
@@ -1,9 +1,9 @@
 story "This is a story", {
     scenario "A scenario in the story", {
-        assert true
+        assert 1 == 1
     }
 
     scenario "Another scenario in the story", {
-        assert false
+        assert 1 == 0
     }
 }
