be06e96270289fa6441e1829dd071926705fb33a
Jonathan Leibiusky <jonathan.leibiusky@mercadolibre.com>
2010-05-20 10:06:43 -0300

    Added rest client plugin

--- test/stories/SomeStory.groovy
+++ test/stories/SomeStory.groovy
@@ -1,6 +1,6 @@
 story "This is a story", {
     scenario "A scenario in the story", {
-        assert 1 == 1
+        twitter = new groovyx.net.http.RESTClient('https://twitter.com/statuses/')
     }
 
     scenario "Another scenario in the story", {
