7477b1341a4452f569058519f4f8c7dbc7d0aa8d
Jonathan Leibiusky <jonathan.leibiusky@mercadolibre.com>
2010-05-27 14:50:47 -0300

    Fixed classnotfound error when executing tests

--- StoriesGrailsPlugin.groovy
+++ StoriesGrailsPlugin.groovy
@@ -7,6 +7,7 @@ class StoriesGrailsPlugin {
     def description = "Stories is a BDD plugin for Grails that aims to make you life easier with functional testing. Stories does't depend on any testing framework out there. It tries to use them all, offerind a way to organize your tests as Stories and Scenarios. What you do inside each scenario is your problem :)... you can use either Selenium, WebTest, HtmlUnit, etc."
     def documentation = "http://github.com/xetorthio/stories"
     def loadAfter = ['core', 'hibernate']
+    def scopes = [excludes:"war"]
     def pluginExcludes = [
         'grails-app/**/*',
         'lib/**/*',
--- scripts/_Install.groovy
+++ scripts/_Install.groovy
@@ -1,1 +1,9 @@
-ant.mkdir(dir:"${basedir}/test/stories")
+import org.codehaus.groovy.grails.plugins.GrailsPluginUtils
+
+includeTargets << grailsScript("_PluginDependencies")
+
+target(install: 'install stories for grails') {
+    ant.mkdir(dir:"${basedir}/test/stories")
+}
+
+install()
