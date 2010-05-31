e9c236356c313dca330357b88434f83daf2028c2
Jonathan Leibiusky <jonathan.leibiusky@mercadolibre.com>
2010-05-31 14:02:22 -0300

    Removed bad test

--- test/stories/SomeStory.groovy
+++ test/stories/SomeStory.groovy
@@ -9,16 +9,6 @@ story "This is a story", {
 
     }
 
-    scenario "A scenario in the story", {
-        def item = new RESTClient('http://localhost:8080/syi/api/items/')
-        def resp = item.get([path: "22050"])
-        def data = resp.data
-        // println "El item Id es ${data.id}"
-        // println "La categ Id es ${data.categId}"
-        assert 200 == resp.status
-        assert 22050 == data.id
-    }
-
     scenario "Another scenario in the story", {
         assert 1 == 0
     }
